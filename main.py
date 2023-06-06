fa9c4afe378cdb8aac2a2e9ada7a29a312cb1202
Raphsinai <66040576+Raphsinai@users.noreply.github.com>
2023-06-06 15:02:19 +0100

    Get vals no nan and test imputation with rand nan

--- main.py
+++ main.py
@@ -29,12 +29,27 @@ def read_csv(filename : str) -> np.array:
                 data[l_index][v_index] = np.nan
     return names, np.array(data)
 
-def get_non_nan(array : np.ndarray) -> np.array:
-    ## TODO ##
-    #
-    # Get columns or rows with least nans and exclude those samples
-    #
-    return
+def get_non_nan(array : np.ndarray, start_n : int = 0, col_n : int = 5) -> np.array:
+    cols = array.transpose()
+    cols_nan = np.isnan(cols)
+    n_cols_nan = [np.count_nonzero(col) for col in cols_nan]
+    m = sorted(n_cols_nan)[start_n:start_n+col_n]
+    col_indicies = []
+    row_indicies = [i for i in range(array.shape[0])]
+    new_cols = []
+    for i, value in enumerate(n_cols_nan):
+            if value in m:
+                col_indicies.append(i)
+                new_cols.append(cols[i])
+    new_rows = np.array(new_cols).transpose()
+    new_array = new_rows.copy()
+    deleted = 0
+    for i, row in enumerate(np.isnan(new_rows)):
+        if np.count_nonzero(row) > 0:
+            new_array = np.delete(new_array, i-deleted, axis=0)
+            row_indicies.remove(i)
+            deleted += 1
+    return row_indicies, col_indicies, new_array
 
 def get_small_nan(array : np.ndarray, min_n : int = 5, square : bool = True, use_mult : bool =False) -> np.array:
     height, width = array.shape
@@ -83,52 +98,71 @@ def get_small_nan(array : np.ndarray, min_n : int = 5, square : bool = True, use
             indexes.append(index)
     return len(indexes), [rows_cols[index] for index in indexes], [segments[index] for index in indexes]
 
-def save_to_sheet(array : np.ndarray, sheet, names : list = None, sample_start : int = 1, highlight : list = []):
+def save_to_sheet(array : np.ndarray, sheet, names : list = None, samples : list = [], highlight : list = []):
+    """
+    Save numpy array to an excel sheet with specified highlighted nan coordinates
+    """
+    # If names of columns are provided add them to the first row
     if names != None:
         names = ['Sample #']+names
         sheet.append(names)
+    # For each row replace nan with 'NA' string
     for n, row in enumerate(array):
         l_row = list(row)
         for i, value in enumerate(np.isnan(row)):
             if value:
                 l_row[i] = 'NA'
-        row = [f'Sample #{n+sample_start}']+l_row
+        row = [f'Sample #{samples[n]}']+l_row
         sheet.append(row)
     for cell in highlight:
         sheet[chr(65+cell[0]+1)+str(cell[1]+2)].fill = PatternFill(start_color='ebe834', end_color='ebe834', fill_type='solid')
     return sheet
     
 
-def test_all(array : np.ndarray, names : list = None, sample_start : int = 1, filename : str = 'Test') -> list:
-    na_indicies = []
-    for r_i, row in enumerate(np.isnan(array)):
-        for c_i, value in enumerate(row):
-            if value:
-                na_indicies.append((c_i, r_i))
+def randints(max : int, n_rand : int, seed : int = 25) -> list:
+    nums = [None for _ in range(n_rand)]
+    for i in range(n_rand):
+        random.seed(seed+i)
+        nums[i] = round(random.random()*max)
+    return nums
+
+
+def test_all(array : np.ndarray, n_nan : int = 1, names : list = None, samples : list = [], filename : str = 'Test', seed : int = 25) -> list:
+    """
+    Test all relevant imputation methods and save them to a spreadsheet
+    """
+    print(array.shape)
+    height, width = array.shape
+    col_indicies = randints(width, n_nan, seed)
+    row_indicies = randints(height, n_nan, seed+n_nan)
+    na_indicies = [(col_indicies[i], row_indicies[i]) for i in range(n_nan)]
+    # Create workbook to store data
     workbook = openpyxl.Workbook()
     sheet = workbook.active
     sheet.title = 'Original'
-    sheet = save_to_sheet(array, sheet, names, sample_start, na_indicies)
+    sheet = save_to_sheet(array, sheet, names, samples, na_indicies)
+    for index in na_indicies:
+        array[index[1], index[0]] = np.nan
     workbook.create_sheet(title='MICE Imputation')
     sheet = workbook['MICE Imputation']
     imputed = impyute.mice(array)
-    sheet = save_to_sheet(imputed, sheet, names, sample_start, na_indicies)
+    sheet = save_to_sheet(imputed, sheet, names, samples, na_indicies)
     workbook.create_sheet(title='k=3 Nearest Neighbours Imputation')
     sheet = workbook['k=3 Nearest Neighbours Imputation']
     imputed = impyute.fast_knn(array, k=3)
-    sheet = save_to_sheet(imputed, sheet, names, sample_start, na_indicies)
+    sheet = save_to_sheet(imputed, sheet, names, samples, na_indicies)
     workbook.create_sheet(title='Expectation Maximisation Imputation')
     sheet = workbook['Expectation Maximisation Imputation']
     imputed = impyute.em(array)
-    sheet = save_to_sheet(imputed, sheet, names, sample_start, na_indicies)
+    sheet = save_to_sheet(imputed, sheet, names, samples, na_indicies)
     workbook.create_sheet(title='Moving Window (Interpolation) Imputation Transposed')
     sheet = workbook['Moving Window (Interpolation) Imputation Transposed']
     imputed = impyute.moving_window(array.transpose())
-    sheet = save_to_sheet(imputed.transpose(), sheet, names, sample_start, na_indicies)
+    sheet = save_to_sheet(imputed.transpose(), sheet, names, samples, na_indicies)
     workbook.create_sheet(title='Moving Window (Interpolation) Imputation')
     sheet = workbook['Moving Window (Interpolation) Imputation']
     imputed = impyute.moving_window(array)
-    sheet = save_to_sheet(imputed, sheet, names, sample_start, na_indicies)
+    sheet = save_to_sheet(imputed, sheet, names, samples, na_indicies)
     workbook.save(f'{filename}.xlsx')
 
     
@@ -138,8 +172,9 @@ def test_all(array : np.ndarray, names : list = None, sample_start : int = 1, fi
 if __name__ == '__main__':
     np.set_printoptions(linewidth=1000)
     names, array = read_csv('sample_data_raf.csv')
-    smallest_nan = get_small_nan(array, min_n=12)
+    # smallest_nan = get_small_nan(array, min_n=12)
+    no_nan = get_non_nan(array, 10)
 
-    print(smallest_nan[2][0])
+    print(no_nan[0], no_nan[1])
     
-    #test_all(smallest_nan[2][0], names[smallest_nan[1][0][0][1]:smallest_nan[1][0][1][1]], smallest_nan[1][0][0][0])
+    test_all(no_nan[2], 2, [names[i] for i in no_nan[1]], no_nan[0],seed=40)
